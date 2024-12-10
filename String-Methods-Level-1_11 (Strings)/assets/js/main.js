e2d57022a659382ef200777fe01668f1c901dfae
Ruslan-S-Code <ruslan_cross@ua.fm>
2024-12-10 13:35:02 +0100

    Tag-17 Javascript

---
+++ String-Methods-Level-1_11 (Strings)/assets/js/main.js
@@ -0,0 +1,21 @@
+let x = "Hello"
+let y = "World"
+
+let z = x + y;
+console.log(z);
+document.write("<p>" + z + "</p>");
+
+
+let n = x + "" + y;
+console.log(n);
+document.write(n);
+
+x += "World"; 
+document.write("<p>" + x + "</p>");
+
+let meinString = "Ich bin Erste:r";
+meinString += " Ich komme auf Platz zwei";
+console.log(meinString);
+document.write(meinString);
+
+
